7bf26320816fd18380373e089866376809adb740
DavidHurst <davidshrst@gmail.com>
2021-12-02 23:35:20 +0000

    Moveing forward with project in the prototyping stage

--- ai-umpire/simulation.py
+++ ai-umpire/simulation.py
@@ -3,16 +3,16 @@ import pychrono.irrlicht as chronoirr
 from pathlib import Path
 import pychrono.postprocess as postprocess
 
-mm = 0.001
-WALL_THICKNESS = 5 * mm
-COURT_LENGTH = 9750 * mm
-WALL_HEIGHT = 5250 * mm
-COURT_WIDTH = 6400 * mm
-BACK_WALL_OUT_LINE_HEIGHT = 2130 * mm
-TIN_HEIGHT = 430 * mm
-LINE_MARKING_WIDTH = 50 * mm
-PAINT_THICKNESS = 1 * mm
-FRONT_WALL_OUT_LINE_HEIGHT = 4570 * mm
+# mm = 0.001
+WALL_THICKNESS = 0.03
+COURT_LENGTH = 9.75
+WALL_HEIGHT = 5.25
+COURT_WIDTH = 6.4
+BACK_WALL_OUT_LINE_HEIGHT = 2.13
+TIN_HEIGHT = 0.43
+LINE_MARKING_WIDTH = 0.05
+PAINT_THICKNESS = 0.001
+FRONT_WALL_OUT_LINE_HEIGHT = 4.57
 
 # Set location of data file
 data_path = str(Path("C:\\Users\\david\\miniconda3\\pkgs\\pychrono-6.0.0-py37_0\\Library\\data\\").resolve())
@@ -29,13 +29,13 @@ sph_mat = chrono.ChMaterialSurfaceNSC()
 sph_mat.SetFriction(0.25)
 
 # Ball
-ball = chrono.ChBodyEasySphere(20 * mm, 0.0001, True, True, sph_mat)
-ball.SetPos(chrono.ChVectorD(0, 100 * mm, COURT_LENGTH * 0.6))
+ball = chrono.ChBodyEasySphere(0.04, 1, True, True, sph_mat)
+ball.SetPos(chrono.ChVectorD(-2, 1, -2.5))
 ball.SetName('Ball')
-ball.SetPos_dt(chrono.ChVectorD(8, 20, 55))  # Speed
-ball.SetPos_dtdt(chrono.ChVectorD(5, 5, 5))  # Acceleration
-ball.SetRot_dt(chrono.ChQuaternionD(0, 0, 0.5, 0))  # Rotation
-ball.SetRot_dtdt(chrono.ChQuaternionD(0, 0, 0.5, 0))  # Rotation acceleration
+ball.SetPos_dt(chrono.ChVectorD(5.5, 6, 40))                # Speed
+ball.SetPos_dtdt(chrono.ChVectorD(0, 2, 3))                 # Acceleration
+# ball.SetRot_dt(chrono.ChQuaternionD(0, 0, 0.5, 0))        # Rotation
+# ball.SetRot_dtdt(chrono.ChQuaternionD(0, 0, 0.5, 0))      # Rotation acceleration
 
 # Floor
 floor = chrono.ChBodyEasyBox(COURT_WIDTH, WALL_THICKNESS, COURT_LENGTH, 1, True, True, surface_mat)
@@ -62,19 +62,19 @@ right_wall.SetPos(chrono.ChVectorD(COURT_WIDTH / 2, WALL_HEIGHT / 2, 0))
 right_wall.SetBodyFixed(True)
 
 # Front wall
-front_wall = chrono.ChBodyEasyBox(COURT_WIDTH, WALL_HEIGHT, WALL_THICKNESS, 1000, True, True, surface_mat)
+front_wall = chrono.ChBodyEasyBox(COURT_WIDTH, WALL_HEIGHT, WALL_THICKNESS, 1, True, True, surface_mat)
 front_wall.SetName('Front Wall')
 front_wall.SetPos(chrono.ChVectorD(0, WALL_HEIGHT / 2, COURT_LENGTH / 2))
 front_wall.SetBodyFixed(True)
 
 # Back wall
-back_wall = chrono.ChBodyEasyBox(COURT_WIDTH, BACK_WALL_OUT_LINE_HEIGHT, WALL_THICKNESS, 1000, True, True, surface_mat)
+back_wall = chrono.ChBodyEasyBox(COURT_WIDTH, BACK_WALL_OUT_LINE_HEIGHT, WALL_THICKNESS, 1, True, True, surface_mat)
 back_wall.SetName('Back Wall')
 back_wall.SetPos(chrono.ChVectorD(0, BACK_WALL_OUT_LINE_HEIGHT / 2, -COURT_LENGTH / 2))
 back_wall.SetBodyFixed(True)
 
 # Front wall line
-front_wall_out_line = chrono.ChBodyEasyBox(COURT_WIDTH, LINE_MARKING_WIDTH, PAINT_THICKNESS, 1000, True, True,
+front_wall_out_line = chrono.ChBodyEasyBox(COURT_WIDTH, LINE_MARKING_WIDTH, PAINT_THICKNESS, 1, True, True,
                                            surface_mat)
 front_wall_out_line.SetName('Front Wall Out-Line')
 front_wall_out_line.SetPos(chrono.ChVectorD(0, FRONT_WALL_OUT_LINE_HEIGHT + (LINE_MARKING_WIDTH / 2),
@@ -82,9 +82,9 @@ front_wall_out_line.SetPos(chrono.ChVectorD(0, FRONT_WALL_OUT_LINE_HEIGHT + (LIN
 front_wall_out_line.SetBodyFixed(True)
 
 # Service line
-service_line = chrono.ChBodyEasyBox(COURT_WIDTH, LINE_MARKING_WIDTH, PAINT_THICKNESS, 1000, True, True, surface_mat)
+service_line = chrono.ChBodyEasyBox(COURT_WIDTH, LINE_MARKING_WIDTH, PAINT_THICKNESS, 1, True, True, surface_mat)
 service_line.SetName('Service Line')
-service_line.SetPos(chrono.ChVectorD(0, (1780 * mm) + (LINE_MARKING_WIDTH / 2),
+service_line.SetPos(chrono.ChVectorD(0, 1.7 + (LINE_MARKING_WIDTH / 2),
                                      (COURT_LENGTH / 2) - (WALL_THICKNESS / 2) - PAINT_THICKNESS))
 service_line.SetBodyFixed(True)
 
@@ -183,47 +183,64 @@ class MyReportContactCallback(chrono.ReportContactCallback):
 reporter = MyReportContactCallback()
 
 # Visualise system with Irrlicht app
-# vis_app = chronoirr.ChIrrApp(system, 'Ball Visualisation', chronoirr.dimension2du(800, 800))
-#
-# vis_app.AddTypicalCamera(chronoirr.vector3df(0, BACK_WALL_OUT_LINE_HEIGHT + 1500 * mm, -COURT_LENGTH))
-# vis_app.AddTypicalSky(chrono.GetChronoDataFile('\\skybox\\'))
-# # vis_app.AddTypicalLights()
-# vis_app.AddLight(chronoirr.vector3df(0, 30, 0), 200)
-# vis_app.AddShadowAll()
-# vis_app.AssetBindAll()
-# vis_app.AssetUpdateAll()
-#
-# # -- Run simulation -- #
-# vis_app.SetTimestep(0.01)
-# vis_app.SetTryRealtime(True)
-#
-# while vis_app.GetDevice().run():
-#     vis_app.BeginScene()
-#     vis_app.DrawAll()
-#     vis_app.DoStep()
-#     vis_app.EndScene()
-#
-#     system.GetContactContainer().ReportAllContacts(reporter)
+vis_app = chronoirr.ChIrrApp(system, 'Ball Visualisation', chronoirr.dimension2du(800, 800))
+
+vis_app.AddTypicalCamera(chronoirr.vector3df(0, BACK_WALL_OUT_LINE_HEIGHT + 1.5, -COURT_LENGTH))
+vis_app.AddTypicalSky(chrono.GetChronoDataFile('\\skybox\\'))
+# vis_app.AddTypicalLights()
+vis_app.AddLight(chronoirr.vector3df(0, 3, 0), 100)
+vis_app.AddShadowAll()
+vis_app.AssetBindAll()
+vis_app.AssetUpdateAll()
+
+time_step = 0.001     # 1000th of a second i.e. 1000fps so can average 20 frames to get 50fps and blur
+
+# -- Run simulation -- #
+vis_app.SetTimestep(time_step)
+vis_app.SetTryRealtime(True)
+system.SetStep(time_step)
+
+while vis_app.GetDevice().run():
+    vis_app.BeginScene()
+    vis_app.DrawAll()
+    vis_app.DoStep()
+    vis_app.EndScene()
+
+    print(f'Current time step = {system.GetChTime()} seconds.')
+    print(f'Step size = {system.GetStep()} seconds.')
+    print(f'Time to calculate one step {system.GetTimerStep()} seconds.')
+    print(f'Ball pos {ball.GetPos()}')
+    print(f'Ball speed {ball.GetPos_dt()}')
+    print(f'')
+
+    system.GetContactContainer().ReportAllContacts(reporter)
 
 # Export simulation data so that it can be visualised in POV-Ray
-pov_exporter = postprocess.ChPovRay(system)
-pov_exporter.SetTemplateFile(f'{data_path}\\_template_POV.pov')
-pov_exporter.SetBasePath(str(Path('C:\\Users\\david\\Downloads\\generated_povray').resolve()))
-pov_exporter.SetCamera(chrono.ChVectorD(0, BACK_WALL_OUT_LINE_HEIGHT + 1500 * mm, -COURT_LENGTH * 1.1),
-                       chrono.ChVectorD(0, WALL_HEIGHT / 2, COURT_LENGTH / 2), 0)
-pov_exporter.SetLight(chrono.ChVectorD(0, 30, 0), chrono.ChColor(1, 1, 1, 1), True)
-pov_exporter.SetBackground(chrono.ChColor(0.2, 0.2, 0.2, 1))
-pov_exporter.SetPictureSize(852, 480)  # 1280, 720
-pov_exporter.SetAntialiasing(True, 6, 0.3)
-
-# Add bodies to export
-pov_exporter.AddAll()
-
-pov_exporter.ExportScript()
-
-# Perform a short simulation
-while system.GetChTime() <= 1:
-    system.DoStepDynamics(0.01)
-    system.GetContactContainer().ReportAllContacts(reporter)
-    print(f'Time Step = {system.GetChTime():.2f}')
-    pov_exporter.ExportData()
+# pov_exporter = postprocess.ChPovRay(system)
+# pov_exporter.SetTemplateFile(f'{data_path}\\_template_POV.pov')
+# pov_exporter.SetBasePath(str(Path('C:\\Users\\david\\Downloads\\generated_povray').resolve()))
+# pov_exporter.SetCamera(chrono.ChVectorD(0, BACK_WALL_OUT_LINE_HEIGHT + 1.5, -COURT_LENGTH * 1.1),
+#                        chrono.ChVectorD(0, WALL_HEIGHT / 2, COURT_LENGTH / 2), 0)
+# pov_exporter.SetLight(chrono.ChVectorD(0, WALL_HEIGHT * 1.1, 0), chrono.ChColor(1, 1, 1, 1), True)
+# pov_exporter.SetBackground(chrono.ChColor(0.2, 0.2, 0.2, 1))
+# pov_exporter.SetPictureSize(852, 480)  # 1280, 720
+# # pov_exporter.SetAntialiasing(True, 6, 0.3)
+# '''
+#     Commands to append to .ini file:
+#         Output_File_Type=J
+#         Quality=8
+#         Continue_Trace=on       # Continue rendering from last frame rendered if render was stopped.
+#         Work_Threads=2048
+# '''
+#
+# # Add bodies to export
+# pov_exporter.AddAll()
+#
+# pov_exporter.ExportScript()
+#
+# # Perform a short simulation
+# while system.GetChTime() <= 1:
+#     system.DoStepDynamics(time_step)
+#     # system.GetContactContainer().ReportAllContacts(reporter)
+#     print(f'Time Step = {system.GetChTime():.2f}')
+#     pov_exporter.ExportData()
