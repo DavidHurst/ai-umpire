8e3711a1d31fcf38aae7c786d2e9439e1daa5a13
DavidHurst <davidshrst@gmail.com>
2022-01-20 14:32:38 +0000

    Minor tuning to detector methods)

--- ai_umpire/localisation/localiser.py
+++ ai_umpire/localisation/localiser.py
@@ -94,9 +94,20 @@ class Localiser:
                 frame, None, 0, 255, cv2.NORM_MINMAX
             ).astype(np.uint8)
 
+            # Detect blobs
             keypoints = detector.detect(frame_normed_uint8)
+
+            # Draw blobs
+            colour_im_path = (
+                    Path(
+                        "C:\\Users\\david\\Data\\AI Umpire DS\\blurred_frames\\sim_0_blurred"
+                    )
+                    / f"frame{str(i).zfill(5)}.jpg"
+            )
+            colour_im = cv2.imread(str(colour_im_path), cv2.IMREAD_GRAYSCALE)
+            display_img = cv2.cvtColor(colour_im, cv2.COLOR_GRAY2BGR)
             im_with_keypoints = cv2.drawKeypoints(
-                frame_normed_uint8,
+                display_img,
                 keypoints,
                 np.array([]),
                 (0, 0, 255),
@@ -104,8 +115,10 @@ class Localiser:
             )
 
             # Show keypoints
-            cv2.imshow(f"Frame {i} with keypoints", im_with_keypoints)
-            cv2.waitKey(0)
+            # cv2.imshow(f"Frame {i} with Keypoints Drawn", im_with_keypoints)
+            # cv2.waitKey(0)
+            # plt.tight_layout()
+            # plt.imsave(f"frame_{str(i).zfill(5)}_simple_blob_filter.jpg", im_with_keypoints)
 
     def localise_ball_hough_circle(
         self, foreground_segmented_frames: np.ndarray
@@ -122,27 +135,36 @@ class Localiser:
 
             # Detect circles
             min_dist_between_circles: float = frame_normed_uint8.shape[0] * 0.1
-            max_radius: int = int(frame_normed_uint8.shape[1] * 0.05)
+            max_radius: int = int(frame_normed_uint8.shape[1] * 0.1)
             print(f"Max radius={max_radius}, min dist={min_dist_between_circles}")
             circles_detected = cv2.HoughCircles(
                 image=frame_normed_uint8,
                 minDist=min_dist_between_circles,
-                method=cv2.HOUGH_GRADIENT,
+                method=cv2.HOUGH_GRADIENT_ALT,
                 dp=1,
-                param1=100,
-                param2=100,
+                param1=400,
+                param2=0.2,
                 minRadius=0,
                 maxRadius=max_radius,
             )
 
             # Draw circles
-            display_img = cv2.cvtColor(frame_normed_uint8, cv2.COLOR_GRAY2BGR)
+            colour_im_path = (
+                Path(
+                    "C:\\Users\\david\\Data\\AI Umpire DS\\blurred_frames\\sim_0_blurred"
+                )
+                / f"frame{str(i).zfill(5)}.jpg"
+            )
+            colour_im = cv2.imread(str(colour_im_path), cv2.IMREAD_GRAYSCALE)
+            display_img = cv2.cvtColor(colour_im, cv2.COLOR_GRAY2BGR)
             if circles_detected is not None:
                 for x, y, r in circles_detected[0]:
                     cv2.circle(display_img, (int(x), int(y)), int(r), (0, 0, 255))
 
-            cv2.imshow(f"Frame {i} with Circles Drawn", display_img)
-            cv2.waitKey(0)
+            # cv2.imshow(f"Frame {i} with Circles Drawn", display_img)
+            # cv2.waitKey(0)
+            # plt.tight_layout()
+            # plt.imsave(f"frame_{str(i).zfill(5)}_hough_circle.jpg", display_img)
 
     def localise_ball_blob_log(self, foreground_segmented_frames: np.ndarray) -> None:
         for i in tqdm(
@@ -155,15 +177,29 @@ class Localiser:
                 frame, None, 0, 255, cv2.NORM_MINMAX
             ).astype(np.uint8)
 
-            # Detect blobs
-            blobs_log = blob_log(frame_normed_uint8, min_sigma=10, num_sigma=10, threshold=0.1)
+            # Detect blobs- can give kernel std devs as sequence per axis maybe to elongate blobs?
+            blobs_log = blob_log(
+                frame_normed_uint8,
+                min_sigma=1,
+                max_sigma=30,
+                num_sigma=10,
+                threshold=0.05,
+            )
 
             # Draw detected blobs
-            display_img = cv2.cvtColor(frame_normed_uint8, cv2.COLOR_GRAY2BGR)
+            colour_im_path = (
+                Path(
+                    "C:\\Users\\david\\Data\\AI Umpire DS\\blurred_frames\\sim_0_blurred"
+                )
+                / f"frame{str(i).zfill(5)}.jpg"
+            )
+            colour_im = cv2.imread(str(colour_im_path), cv2.IMREAD_GRAYSCALE)
+            display_img = cv2.cvtColor(colour_im, cv2.COLOR_GRAY2BGR)
             for blob in blobs_log:
                 y, x, r = blob
-
                 cv2.circle(display_img, (int(x), int(y)), int(r), (0, 0, 255))
 
-            cv2.imshow(f"Frame {i} with Circles Drawn", display_img)
-            cv2.waitKey(0)
+            # cv2.imshow(f"Frame {i} with Circles Drawn", display_img)
+            # cv2.waitKey(0)
+            # plt.tight_layout()
+            # plt.imsave(f"frame_{str(i).zfill(5)}_log.jpg", display_img)
